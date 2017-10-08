6091feba8cc63fcebd321345298cc0d1750d31d2
godruoyi <godruoyi@gmail.com>
2017-10-08 19:24:47 +0800

    add aliyun provider

---
+++ src/Aliyun/AppCode.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Godruoyi\OCR\Aliyun;
+
+class AppCode
+{
+    /**
+     * Aliyun App Code
+     * 
+     * @var string
+     */
+    protected $appCode;
+
+    /**
+     * Register App Code
+     * 
+     * @param string $appCode
+     */
+    public function __construct($appCode)
+    {
+        $this->appCode = $appCode;
+    }
+
+    /**
+     * Get App code
+     * 
+     * @return string
+     */
+    public function getAppCode()
+    {
+        return $this->appCode;
+    }
+
+    /**
+     * Get App Code with Header
+     * 
+     * @return array
+     */
+    public function getAppCodeHeader()
+    {
+        return [
+            'Authorization' => 'APPCODE ' . $this->getAppCode()
+        ];
+    }
+}
---
+++ src/Aliyun/OCRManager.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Godruoyi\OCR\Aliyun;
+
+/**
+ * @see https://data.aliyun.com/product/ocr
+ *
+ * 
+ */
+class OCRManager
+{
+    /**
+     * 印刷文字识别_身份证识别
+     * 
+     * @param string|SplFIleInfo $images
+     * @param array  $options
+     */
+    public function FunctionName($images, array $options = [])
+    {
+        # code...
+    }
+}
