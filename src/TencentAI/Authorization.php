144214e03a6030dada2e4aa1de72afa8fd6c02f7
leohu <cqleohu@qq.com>
2017-12-06 17:50:38 +0800

    添加Tencent AI 的OCR接口

---
+++ src/TencentAI/Authorization.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Godruoyi\OCR\TencentAI;
+
+/**
+ * @see https://ai.qq.com/doc/auth.shtml
+ * @author leohu <alpha1130@gmail.com>
+ *
+ */
+class Authorization
+{
+
+    /**
+     * Tencent AI appid
+     *
+     * @var string
+     */
+    protected $appId = '';
+
+    /**
+     * Tencent AI appkey
+     *
+     * @var string
+     */
+    protected $appKey = '';
+
+    /**
+     * Authorization constructor.
+     *
+     * @param $appId
+     * @param $appKey
+     */
+    public function __construct($appId, $appKey)
+    {
+        $this->appId = $appId;
+        $this->appKey = $appKey;
+    }
+
+    /**
+     * append signature to params
+     *
+     * @param array $params
+     * @param $timestamp
+     * @param $noncestr
+     *
+     * @return array
+     */
+    public function appendSignature(array $params = [], $timestamp = '', $noncestr = '')
+    {
+        $params += [
+            'app_id' => $this->appId,
+            'time_stamp' => $timestamp ? : time(),
+            'nonce_str' => $noncestr ? : md5(uniqid())
+        ];
+
+        if(isset($params['app_key'])) {
+            unset($params['app_key']);
+        }
+
+        ksort($params);
+        $params['sign'] = strtoupper(md5(http_build_query($params + ['app_key' => $this->appKey])));
+
+        return $params;
+    }
+}
