88307d3b0e02bba54e6eb8ef58eda306f9e87adc
leohu <cqleohu@qq.com>
2017-12-06 18:35:30 +0800

    补完TencentAIProvider

---
+++ src/Providers/TencentAIProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the godruoyi/ocr.
+ *
+ * (c) godruoyi <godruoyi@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Godruoyi\OCR\Providers;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Godruoyi\OCR\TencentAI\OCRManager;
+use Godruoyi\OCR\TencentAI\Authorization;
+
+class TencentAIProvider implements ServiceProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function register(Container $pimple)
+    {
+        $pimple['tencentai.auth'] = function ($app) {
+            return new Authorization(
+                $app['config']->get('ocrs.tencentai.app_id'),
+                $app['config']->get('ocrs.tencentai.app_key')
+            );
+        };
+
+        $pimple['tencentai'] = function ($app) {
+            return new OCRManager($app['tencentai.auth']);
+        };
+    }
+}
