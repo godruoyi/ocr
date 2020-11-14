88cf4b4c386ace9b3c0cb1f60431fe681cc42609
Godruoyi <godruoyi@163.com>
2020-11-14 18:47:19 +0800

    🎈 publish version 2.0.0

--- tests/Support/TencentSignatureV3Test.php
+++ tests/Support/TencentSignatureV3Test.php
@@ -28,14 +28,6 @@ class TencentSignatureV3Test extends TestCase
         $this->assertSame($signer->hashedRequestPayload($body), '35e9c5b0e3ae67532d3c9f17ead6c90222632e5b1ff7f6e89887f1398934f064');
     }
 
-    public function testHashedRequestPayload2()
-    {
-        $signer = new TencentSignatureV3($this->secretId, $this->secretKey);
-        $body = '{"ImageUrl":"https://cloud.tencent.com/1"}';
-
-        $this->assertSame($signer->hashedRequestPayload($body), 'e3d1d29a998fcb1d83bb752972a15a6660041c7b112a481de8aedaa9e2e2729b');
-    }
-
     public function testCanonicalRequest()
     {
         $signer = new TencentSignatureV3($this->secretId, $this->secretKey);
@@ -45,6 +37,9 @@ class TencentSignatureV3Test extends TestCase
             'https://cvm.tencentcloudapi.com',
             [
                 'content-type' => ['application/json; charset=utf-8'],
+                'headers'      => [
+                    'X-TC-Timestamp' => '1551113065',
+                ],
             ],
             '{"Limit": 1, "Filters": [{"Values": ["\u672a\u547d\u540d"], "Name": "instance-name"}]}',
         );
@@ -52,22 +47,4 @@ class TencentSignatureV3Test extends TestCase
         $this->assertSame($signer->hashedRequestPayload($request->getBody()), '35e9c5b0e3ae67532d3c9f17ead6c90222632e5b1ff7f6e89887f1398934f064');
         $this->assertSame($signer->canonicalRequest($request), "POST\n/\n\ncontent-type:application/json; charset=utf-8\nhost:cvm.tencentcloudapi.com\n\ncontent-type;host\n35e9c5b0e3ae67532d3c9f17ead6c90222632e5b1ff7f6e89887f1398934f064");
     }
-
-    // public function testAuthorization()
-    // {
-    //     $signer = new TencentSignatureV3($this->secretId, $this->secretKey);
-    //     $request = new \GuzzleHttp\Psr7\Request(
-    //         'post',
-    //         'https://cvm.tencentcloudapi.com',
-    //         [
-    //             'content-type' => ['application/json; charset=utf-8'],
-    //             'X-TC-Timestamp' => 1551113065,
-    //         ],
-    //         '{"Limit": 1, "Filters": [{"Values": ["\u672a\u547d\u540d"], "Name": "instance-name"}]}',
-    //     );
-
-    //     $a = $signer->authorization($request);
-
-    //     var_dump($a);
-    // }
 }
